a8f828582051c5123fde0b8a4c8d8d1bd608471c
hjssin <46236470+hjssin@users.noreply.github.com>
2025-12-31 15:51:04 +0900

    add envelope open steps

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 window.onload = () => {
-  document.querySelector('.envelope')
-    .classList.add('open');
+  const scene = document.querySelector('.scene');
+
+  const steps = [
+    'step-1',
+    'step-2',
+    'step-3',
+    'step-4',
+    'step-5'
+  ];
+
+  steps.forEach((step, i) => {
+    setTimeout(() => {
+      scene.className = `scene ${step}`;
+    }, i * 900);
+  });
 };
